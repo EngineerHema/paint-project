f67a9f4fe742e41cf05c68481d8a95b333fc2949
kero <kerosamy42004@gmail.com>
2024-11-09 00:45:36 +0200

    base files created

---
+++ Paint-Back-end/src/test/java/Paint/example/Paint_Back_end/PaintBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package Paint.example.Paint_Back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
