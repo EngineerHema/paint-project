4eef5f088dc32b9a0ea5607782ffafe51a049d57
EngineerHema <ibrahimabdelhalim4@gmail.com>
2024-12-02 05:05:17 +0200

    v2.0

---
+++ Paint-Back-end/Backend/src/main/java/John/William/fullStackCalculator/shapeApplication.java
@@ -0,0 +1,16 @@
+package John.William.fullStackCalculator;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class shapeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(shapeApplication.class, args);
+
+	}
+}
+
